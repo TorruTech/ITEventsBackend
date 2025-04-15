1cb273e89bcf66a424f9298fc7b1ba343daa8107
TorruTech <javiertp97@gmail.com>
2025-04-15 16:27:28 +0200

    Initial commit

---
+++ src/main/java/com/itevents/main/IteventsApplication.java
@@ -0,0 +1,12 @@
+package com.itevents.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteventsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/itevents/itevents/IteventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itevents.itevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
