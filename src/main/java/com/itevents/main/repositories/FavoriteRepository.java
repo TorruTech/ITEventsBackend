ff05dbff9341c7b35ef661087a68d215149c30d7
TorruTech <javiertp97@gmail.com>
2025-04-19 13:39:15 +0200

    FavoriteController and Repository modified with new endpoints. Now users can add and delete favortes and query all their favorite events

--- src/main/java/com/itevents/main/repositories/FavoriteRepository.java
+++ src/main/java/com/itevents/main/repositories/FavoriteRepository.java
@@ -4,7 +4,10 @@ import com.itevents.main.models.FavoriteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<FavoriteModel, Long> {
     List<FavoriteModel> findByUserId(Long userId);
+    Optional<FavoriteModel> findByUserIdAndEventId(Long userId, Long eventId);
+    boolean existsByUserIdAndEventId(Long userId, Long eventId);
 }
